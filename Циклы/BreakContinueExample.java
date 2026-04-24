c966d438ca5ed8a138c61c771fa1377185a207f1
mtachilina <tachilina.ma@edu.spbstu.ru>
2026-04-24 15:47:32 +0300

    Update BreakContinueExample.java

--- Циклы/BreakContinueExample.java
+++ Циклы/BreakContinueExample.java
@@ -1,10 +1,30 @@
+/**
+ * Программа для демонстрации операторов break и continue в циклах.
+ * 
+ * break - полностью прекращает выполнение цикла.
+ * continue - пропускает текущую итерацию и переходит к следующей.
+ * 
+ * @author Тачилина Мария
+ * @version 1.0
+ * @since 2026-04-24
+ */
 public class BreakContinueExample {
+    
+    /**
+     * Главный метод программы.
+     * Демонстрирует работу break и continue на примере цикла for.
+     * 
+     * При break: цикл останавливается при достижении i = 5.
+     * При continue: выводятся все числа от 1 до 10 кроме 5.
+     * 
+     * @param args аргументы командной строки (не используются)
+     */
     public static void main(String[] args) {
         
         System.out.println(" break");
         for (int i = 1; i <= 10; i++) {
             if (i == 5) {
-                break;
+                break;   // выход из цикла при i = 5
             }
             System.out.print(i + " ");
         }
@@ -12,7 +32,7 @@ public class BreakContinueExample {
         System.out.println("\n continue");
         for (int i = 1; i <= 10; i++) {
             if (i == 5) {
-                continue;
+                continue;   // пропуск итерации при i = 5
             }
             System.out.print(i + " ");
         }
