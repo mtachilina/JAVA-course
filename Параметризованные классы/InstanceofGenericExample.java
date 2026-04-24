444aab88959e3457cb4b71d576f3759747d37637
mtachilina <tachilina.ma@edu.spbstu.ru>
2026-04-24 15:53:49 +0300

    Update InstanceofGenericExample.java

--- Параметризованные классы/InstanceofGenericExample.java
+++ Параметризованные классы/InstanceofGenericExample.java
@@ -1,34 +1,61 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Программа для демонстрации использования оператора instanceof
+ * с параметризованными (generic) классами.
+ * 
+ * Из-за стирания типов (type erasure) в Java невозможно проверить
+ * конкретный параметр типа. Можно использовать только сырые типы
+ * или wildcard <?>.
+ * 
+ * @author Тачилина Мария
+ * @version 1.0
+ * @since 2026-04-24
+ */
 public class InstanceofGenericExample {
+    
+    /**
+     * Главный метод программы.
+     * Демонстрирует возможность и ограничения instanceof с generic-типами.
+     * 
+     * @param args аргументы командной строки (не используются)
+     */
     public static void main(String[] args) {
         
         List<String> stringList = new ArrayList<>();
         List<Integer> integerList = new ArrayList<>();
         
-        // instanceof с сырым типом (raw type)
+        // instanceof с сырым типом (raw type) - работает
         System.out.println(stringList instanceof List); 
         
-        // instanceof с параметризованным типом - ошибка компиляции
-        // System.out.println(stringList instanceof List<String>); 
+        // instanceof с конкретным параметризованным типом - ошибка компиляции
+        // System.out.println(stringList instanceof List<String>); // ошибка
         
-        // Обход: использовать wildcard
+        // Решение: использовать wildcard <?>
         System.out.println(stringList instanceof List<?>);  
         System.out.println(integerList instanceof List<?>); 
         
-        // С пользовательским классом
+        // С пользовательским generic-классом
         Wrapper<String> wrapperString = new Wrapper<>();
         Wrapper<Integer> wrapperInteger = new Wrapper<>();
         
+        // Сырой тип - работает
         System.out.println(wrapperString instanceof Wrapper);  
+        
+        // Wildcard - работает
         System.out.println(wrapperString instanceof Wrapper<?>); 
         
-        // Ошибка: нельзя проверить конкретный тип параметра
-        // System.out.println(wrapperString instanceof Wrapper<String>); 
+        // Конкретный параметр типа - ошибка компиляции
+        // System.out.println(wrapperString instanceof Wrapper<String>); // ошибка
     }
 }
 
+/**
+ * Параметризованный класс-обёртка.
+ * 
+ * @param <T> тип хранимого значения
+ */
 class Wrapper<T> {
     private T item;
     
