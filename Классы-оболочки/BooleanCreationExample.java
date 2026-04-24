9f323b599154e5abd65eb7f459215c34711e1995
mtachilina <tachilina.ma@edu.spbstu.ru>
2026-04-24 15:43:21 +0300

    Update BooleanCreationExample.java

--- Классы-оболочки/BooleanCreationExample.java
+++ Классы-оболочки/BooleanCreationExample.java
@@ -1,13 +1,35 @@
+/**
+ * Программа для демонстрации всех способов создания объектов типа Boolean.
+ * 
+ * Класс Boolean является классом-оболочкой для примитивного типа boolean.
+ * 
+ * @author Тачилина Мария
+ * @version 1.0
+ * @since 2026-04-24
+ */
 public class BooleanCreationExample {
+    
+    /**
+     * Главный метод программы.
+     * Демонстрирует пять способов создания Boolean объектов:
+     * 
+     * 1. Boolean.valueOf(boolean) - возвращает объект Boolean из примитива
+     * 2. Boolean.valueOf(String) - преобразует строку в Boolean
+     * 3. Boolean.parseBoolean(String) - возвращает примитив boolean
+     * 4. Автоупаковка - автоматическое преобразование примитива в объект
+     * 5. Конструктор new Boolean() (устаревший, не рекомендуется)
+     * 
+     * @param args аргументы командной строки (не используются)
+     */
     public static void main(String[] args) {
         
-        //1
+        // Способ 1: valueOf(boolean)
         Boolean bool1 = Boolean.valueOf(true);
         Boolean bool2 = Boolean.valueOf(false);
         System.out.println("Boolean.valueOf(true): " + bool1);
         System.out.println("Boolean.valueOf(false): " + bool2);
         
-        //2
+        // Способ 2: valueOf(String)
         Boolean bool3 = Boolean.valueOf("true");
         Boolean bool4 = Boolean.valueOf("false");
         Boolean bool5 = Boolean.valueOf("TRUE");    
@@ -18,18 +40,18 @@ public class BooleanCreationExample {
         System.out.println("Boolean.valueOf(\"TRUE\"): " + bool5);
         System.out.println("Boolean.valueOf(\"anything\"): " + bool7);
         
-        //3
+        // Способ 3: parseBoolean (возвращает примитив)
         boolean primitive1 = Boolean.parseBoolean("true");
         boolean primitive2 = Boolean.parseBoolean("false");
         System.out.println("\nBoolean.parseBoolean(\"true\"): " + primitive1);
         System.out.println("Boolean.parseBoolean(\"false\"): " + primitive2);
         
-        //4
+        // Способ 4: автоупаковка (boxing)
         Boolean bool8 = true;
         Boolean bool9 = false;
         System.out.println("\nАвтоупаковка: true -> " + bool8);
         
-        //5
+        // Способ 5: конструктор (устаревший, не рекомендуется к использованию)
         @SuppressWarnings("deprecation")
         Boolean bool10 = new Boolean(true);
         System.out.println("new Boolean(true): " + bool10);
